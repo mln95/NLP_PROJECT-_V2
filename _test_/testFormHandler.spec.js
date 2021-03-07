653e5d2ee4a2ebf3b8189a8d3323d53573658102
mln95 <75915496+mln95@users.noreply.github.com>
2021-03-07 22:52:26 +0100

    Delete testFormHandler.spec.js

--- _test_/testFormHandler.spec.js
+++
@@ -1,7 +0,0 @@
-// import {formHandler} from '../src/client/js/formHandler'
-// const clientData = require('./client_data');
-// describe('Test of function existance', () => {
-//     test('Test if client_data exist', () => {
-//         expect(client_data).toBe(true);
-//     }) 
-// })  
